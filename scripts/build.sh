153049b6f7c105ef7de4b066ce1e2b52b302aa98
Julien Pilet <julien.pilet@opticode.ch>
2020-02-27 16:57:30 +0100

    push images while building others

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,7 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 for image in "redis-look" "redis-sentinel" "redis-utils"; do
 	echo "Building $image"
 	docker build -t destinationbook/${image}:${TAG} "${ROOT}/${image}"
-	docker push destinationbook/${image}:${TAG}
+	docker push destinationbook/${image}:${TAG} &
 done
+
+wait
