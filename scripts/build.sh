92aee6b9c90df0be1e7c6e197e88a52dff3d0bdf
ole kristensen <ole@kristensen.name>
2020-06-22 11:55:30 +0200

    Redis 6.0 and 3 replicas

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,8 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 for image in "redis-look" "redis-sentinel" "redis-utils"; do
 	echo "Building $image"
-	docker build -t destinationbook/${image}:${TAG} "${ROOT}/${image}"
-	docker push destinationbook/${image}:${TAG} &
+	docker build -t docker.io/olekristensen/${image}:${TAG} "${ROOT}/${image}"
+	docker push docker.io/olekristensen/${image}:${TAG} &
 done
 
 wait
--- scripts/test_scale.sh
+++ scripts/test_scale.sh
@@ -7,20 +7,20 @@ REDIS_MASTER_NAME="redismaster"
 export TAG=${1:-"latest"}
 
 echo "Scale redis"
-docker service scale cache_redis-sentinel=5
+docker service scale cache_redis-sentinel=3
 
-until [ "$(docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
-	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME value num-other-sentinels)" = "4" ]; do
+until [ "$(docker run --rm --network redis olekristensen/redis-utils:$TAG \
+	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME value num-other-sentinels)" = "2" ]; do
 	echo "Sentinels not set up yet - sleeping"
 	sleep 2
 done
 
-docker service scale cache_redis=5
+docker service scale cache_redis=3
 
 echo "Make sure the number of slaves are set"
 docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
-	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME reset "num-slaves" 4
+	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME reset "num-slaves" 2
 
 echo "Starting following tests"
 docker run --rm --network redis --volumes-from scripts \
-	redis:4.0.11-alpine sh /scripts/check_scaling.sh 4 4
+	redis:4.0.11-alpine sh /scripts/check_scaling.sh 3 3
