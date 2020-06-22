65321def8a5a881f1c0271d96b81ef0c5377616c
ole kristensen <ole@kristensen.name>
2020-06-22 12:38:13 +0200

    3 nodes

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,8 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 for image in "redis-look" "redis-sentinel" "redis-utils"; do
 	echo "Building $image"
-	docker build -t docker.io/olekristensen/${image}:${TAG} "${ROOT}/${image}"
-	docker push docker.io/olekristensen/${image}:${TAG} &
+	docker build -t olekristensen/${image}:${TAG} "${ROOT}/${image}"
+	docker push olekristensen/${image}:${TAG} &
 done
 
 wait
--- scripts/test_scale.sh
+++ scripts/test_scale.sh
@@ -18,9 +18,9 @@ done
 docker service scale cache_redis=3
 
 echo "Make sure the number of slaves are set"
-docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
+docker run --rm --network redis olekristensen/redis-utils:$TAG \
 	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME reset "num-slaves" 2
 
 echo "Starting following tests"
 docker run --rm --network redis --volumes-from scripts \
-	redis:4.0.11-alpine sh /scripts/check_scaling.sh 3 3
+	redis:4.0.11-alpine sh /scripts/check_scaling.sh 2 2
